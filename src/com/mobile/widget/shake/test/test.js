03d6ae0a085dbe552225e79afc5da5d822726b0c
guochaohui <guochaohui@anxin365.com>
2015-06-12 17:53:05 +0800

    注册成功页面

--- src/com/mobile/widget/shake/test/test.js
+++
@@ -1,9 +0,0 @@
-var Shake = require('com/mobile/widget/shake/shake.js').shake;
-var tip = require('com/mobile/widget/toast.js').show;
-
-exports.shake = Shake.extend({
-    onShake: function() {
-        tip('摇一摇生效！', 2000);
-        // this.stopListening();
-    }
-});
