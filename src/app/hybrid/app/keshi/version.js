3de4a7ca40e9cc54b0afffda09c04b57f8c8fb4a
陈朝阳 <chenchaoyang@iyuesao.com>
2015-08-10 14:28:22 +0800

    框架修改

---
+++ src/app/hybrid/app/keshi/version.js
@@ -0,0 +1,9 @@
+{
+    "defaultVersionSuffix": "0",
+    "expire": 604800,
+    "files": [
+        "app/hybrid/app/keshi/**/*",
+        "app/hybrid/common/*",
+        "app/hybrid/common/**/*"
+    ]
+}
--- src/com/g/config.js
+++ src/com/g/config.js
@@ -5,8 +5,9 @@
         baseUrl: 'http://s1.anxinsta.com/',
         map: [
             [/^(.*\/\/)((.*)\.(js|css|tpl))$/, function (url, server, path, filename, ext) {
+                var key = path.replace('s1.anxinsta.com/', '');
                 var versions = G.config('version') || {};
-                var version = versions[path] || G.config('defaultVersion') || parseInt(defaultVersion + '0', 10);
+                var version = versions[key] || G.config('defaultVersion') || parseInt(defaultVersion + '0', 10);
 
                 return server + filename + '.__' + version + '__.' + ext;
             }]
